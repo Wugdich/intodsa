aa4ec7b3accd7b216a834edcb11ddd316d349e87
Wugd <sintes4ek@gmail.com>
2023-04-27 11:37:55 +0300

    Add 'MenuItem ' model

--- intodsa/menu/models.py
+++ intodsa/menu/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 
-class Menu(models.Model):
-    # fields
-
+class MenuItem(models.Model):
+    title = models.CharField(max_length=20)
+    url = models.URLField()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return
+        return self.title
